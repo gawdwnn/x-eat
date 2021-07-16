3857d1c54c0882d77f14be6f3738ae26250e6b6a
smarttin <smarttin77@gmail.com>
2021-07-16 23:47:29 +0100

    implement local only fields

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
+import { LoggedOutRouter } from "./routers/logged-out-router";
+import { LoggedInRouter } from "./routers/logged-in-router"; 
 
 function App() {
-  return (
-    <div className='text-center'>
-      <h1>Hello</h1>
-    </div>
-  );
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
-
 export default App;
