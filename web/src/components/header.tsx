d491cf5b396281878f1cd6152343590fb90bb24c
smarttin <smarttin77@gmail.com>
2021-07-17 18:56:26 +0100

    edit-profile

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useMe } from "../hooks/useMe";
-import nuberLogo from "../images/logo.svg";
+import logo from "../images/logo.svg";
 
 export const Header: React.FC = () => {
   const { data } = useMe();
   return (
     <>
       {!data?.me.verified && (
-        <div className="bg-red-500 p-3 text-center text-xs text-white">
+        <div className="bg-red-500 p-3 text-center text-base text-white">
           <span>Please verify your email.</span>
         </div>
       )}
       <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-          <img src={nuberLogo} className="w-24" alt="Nuber Eats" />
+          <Link to="/">
+            <img src={logo} className="w-34" alt="Eats" />
+          </Link>
           <span className="text-xs">
             <Link to="/edit-profile">
               <FontAwesomeIcon icon={faUser} className="text-xl" />
