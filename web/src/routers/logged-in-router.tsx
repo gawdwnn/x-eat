f2ae06b76e199e42e71729a70202be87562f4fa7
smarttin <smarttin77@gmail.com>
2021-07-17 14:48:27 +0100

    router and 404

--- web/src/routers/logged-in-router.tsx
+++ web/src/routers/logged-in-router.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import { Restaurants } from "../pages/client/restaurants";
 import { meQuery } from "../__generated__/meQuery";
+
+const ClientRoutes = [
+  <Route path="/" exact>
+    <Restaurants />
+  </Route>,
+];
 
 const ME_QUERY = gql`
   query meQuery {
@@ -23,8 +31,11 @@ export const LoggedInRouter = () => {
     );
   }
   return (
-    <div>
-      <h1>{data.me.email}</h1>
-    </div>
+    <Router>
+      <Switch>
+        {data.me.role === "Client" && ClientRoutes}
+        <Redirect from="/potato" to="/" />
+      </Switch>
+    </Router>
   );
 };
