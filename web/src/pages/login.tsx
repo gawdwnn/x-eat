f2cd61e894de781130275aae9a4d01de9a5027bc
smarttin <smarttin77@gmail.com>
2021-07-17 09:00:08 +0100

    uber eats login UI clone

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,7 +1,10 @@
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
+import { Link } from "react-router-dom";
+import { Button } from "../components/button";
 import { LoginMutation, LoginMutationVariables } from "../__generated__/LoginMutation";
+import nuberLogo from "../images/logo.svg";
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($loginInput: LoginInput!) {
@@ -19,8 +22,10 @@ interface ILoginForm {
 }
 
 export const Login = () => {
-  const { register, formState, getValues, handleSubmit } = useForm<ILoginForm>();
-  const { errors } = formState;
+  const { register, formState, getValues, handleSubmit } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
+  const { errors, isValid } = formState;
 
   const onCompleted = (data: LoginMutation) => {
     const {
@@ -53,15 +58,16 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} alt='title' className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">Welcome back</h4>
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 w-full mb-5">
           <input
             {...register("email", { required: "Email is required" })}
             placeholder="Email"
             required
-            className="input mb-3"
+            className="input mb-3 w-full"
           />
           {errors.email?.message && <FormError errorMessage={errors.email?.message} />}
           <input
@@ -76,11 +82,21 @@ export const Login = () => {
           {errors.password?.type === "minLength" && (
             <FormError errorMessage="Password must be more than 5 chars." />
           )}
-          <button disabled={loading} className="mt-3 btn">{loading ? "Loading..." : "Log In"}</button>
+          <Button
+            canClick={isValid}
+            loading={loading}
+            actionText={"Log in"}
+          />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
         </form>
+        <div>
+          New to Nuber?{" "}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
