5151d2eaa14d608a7a4736e9be4d148752db826b
smarttin <smarttin77@gmail.com>
2021-07-17 09:27:23 +0100

    create account mutation and form design

--- web/src/pages/create-account.tsx
+++ web/src/pages/create-account.tsx
@@ -1,5 +1,125 @@
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import Helmet from "react-helmet";
+import { useForm } from "react-hook-form";
+import { Link, useHistory } from "react-router-dom";
+import { Button } from "../components/button";
+import { FormError } from "../components/form-error";
+import nuberLogo from "../images/logo.svg";
+import { EmailPattern } from "../utils/constants";
+import {
+  createAccountMutation,
+  createAccountMutationVariables,
+} from "../__generated__/createAccountMutation";
+import { UserRole } from "../__generated__/globalTypes";
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccountMutation($createAccountInput: CreateAccountInput!) {
+    createAccount(input: $createAccountInput) {
+      ok
+      error
+    }
+  }
+`;
+
+interface ICreateAccountForm {
+  email: string;
+  password: string;
+  role: UserRole;
+}
 export const CreateAccount = () => {
-  return <span>Create Account</span>;
+  const { register, getValues, handleSubmit, formState } =
+    useForm<ICreateAccountForm>({
+      mode: "onChange",
+      defaultValues: {
+        role: UserRole.Client,
+      },
+    });
+
+  const { errors, isValid } = formState;
+  const history = useHistory();
+
+  const onCompleted = (data: createAccountMutation) => {
+    const {
+      createAccount: { ok },
+    } = data;
+    if (ok) {
+      history.push("/login");
+    }
+  };
+
+  const [createAccountMutation, { loading, data: createAccountMutationResult }] =
+    useMutation<createAccountMutation, createAccountMutationVariables>(
+      CREATE_ACCOUNT_MUTATION,
+      {
+        onCompleted,
+      }
+    );
+
+  const onSubmit = () => {
+    if (!loading) {
+      const { email, password, role } = getValues();
+      createAccountMutation({
+        variables: {
+          createAccountInput: { email, password, role },
+        },
+      });
+    }
+  };
+
+  return (
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Create Account | Nuber Eats</title>
+      </Helmet>
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} alt="title" className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">Let's get started</h4>
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 w-full mb-5">
+          <input
+            {...register("email", {
+              required: "Email is required",
+              pattern: EmailPattern,
+            })}
+            required
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          {errors.email?.message && <FormError errorMessage={errors.email?.message} />}
+          {errors.email?.type === "pattern" && (
+            <FormError errorMessage={"Please enter a valid email"} />
+          )}
+          <input
+            {...register("password", { required: "Password is required" })}
+            required
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          {errors.password?.type === "minLength" && (
+            <FormError errorMessage="Password must be more than 10 chars." />
+          )}
+          <select {...register("role", { required: true })} className="input">
+            {Object.keys(UserRole).map((role, index) => (
+              <option key={index}>{role}</option>
+            ))}
+          </select>
+          <Button canClick={isValid} loading={loading} actionText={"Create Account"} />
+          {createAccountMutationResult?.createAccount.error && (
+            <FormError errorMessage={createAccountMutationResult.createAccount.error} />
+          )}
+        </form>
+        <div>
+          Already have an account?{" "}
+          <Link to="/login" className="text-lime-600 hover:underline">
+            Log in now
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,10 +1,13 @@
 import { gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
+import Helmet from "react-helmet";
 import { FormError } from "../components/form-error";
 import { Link } from "react-router-dom";
 import { Button } from "../components/button";
 import { LoginMutation, LoginMutationVariables } from "../__generated__/LoginMutation";
 import nuberLogo from "../images/logo.svg";
+import { EmailPattern } from "../utils/constants";
+import { isLoggedInVar } from "../apollo";
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($loginInput: LoginInput!) {
@@ -33,6 +36,7 @@ export const Login = () => {
     } = data;
     if (ok) {
       console.log(token);
+      isLoggedInVar(true);
     }
   };
 
@@ -59,17 +63,26 @@ export const Login = () => {
 
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Login | Nuber Eats</title>
+      </Helmet>
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
-        <img src={nuberLogo} alt='title' className="w-52 mb-10" />
+        <img src={nuberLogo} alt="title" className="w-52 mb-10" />
         <h4 className="w-full font-medium text-left text-3xl mb-5">Welcome back</h4>
         <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 w-full mb-5">
           <input
-            {...register("email", { required: "Email is required" })}
+            {...register("email", {
+              required: "Email is required",
+              pattern: EmailPattern,
+            })}
             placeholder="Email"
             required
             className="input mb-3 w-full"
           />
           {errors.email?.message && <FormError errorMessage={errors.email?.message} />}
+          {errors.email?.type === "pattern" && (
+            <FormError errorMessage={"Please enter a valid email"} />
+          )}
           <input
             {...register("password", { required: "Password is required", minLength: 5 })}
             placeholder="Password"
@@ -82,11 +95,7 @@ export const Login = () => {
           {errors.password?.type === "minLength" && (
             <FormError errorMessage="Password must be more than 5 chars." />
           )}
-          <Button
-            canClick={isValid}
-            loading={loading}
-            actionText={"Log in"}
-          />
+          <Button canClick={isValid} loading={loading} actionText={"Log in"} />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
