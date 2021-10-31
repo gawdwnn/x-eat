c344c5221a4e9caef08378bf7585b91e8baf0992
smarttin <smarttin77@gmail.com>
2021-10-31 19:35:47 +0100

    dish options and dish component

--- web/src/pages/owner/my-restaurant.tsx
+++ web/src/pages/owner/my-restaurant.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 import { Helmet } from "react-helmet";
 import { Link, useParams } from "react-router-dom";
+import { Dish } from "../../components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { myRestaurant, myRestaurantVariables } from "../../__generated__/myRestaurant";
 
@@ -35,8 +36,6 @@ export const MyRestaurant = () => {
     },
   });
 
-  console.log({ data });
-
   return (
     <div>
       <Helmet>
@@ -64,7 +63,17 @@ export const MyRestaurant = () => {
         <div className="mt-10">
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) : null}
+          ) : (
+            <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.myRestaurant.restaurant?.menu.map((dish) => (
+                <Dish
+                  name={dish.name}
+                  description={dish.description}
+                  price={dish.price}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
