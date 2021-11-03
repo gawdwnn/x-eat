60b1aa79616c7a2baa26b1d31a6f66609873be15
smarttin <smarttin77@gmail.com>
2021-11-04 00:05:21 +0100

    integrate victory charts to visualise orders

--- web/src/pages/owner/my-restaurant.tsx
+++ web/src/pages/owner/my-restaurant.tsx
@@ -2,8 +2,17 @@ import { gql, useQuery } from "@apollo/client";
 import { Helmet } from "react-helmet";
 import { Link, useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
-import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
+import { DISH_FRAGMENT, ORDERS_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { myRestaurant, myRestaurantVariables } from "../../__generated__/myRestaurant";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLine,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from "victory";
 
 export const MY_RESTAURANT_QUERY = gql`
   query myRestaurant($input: MyRestaurantInput!) {
@@ -15,11 +24,15 @@ export const MY_RESTAURANT_QUERY = gql`
         menu {
           ...DishParts
         }
+        orders {
+          ...OrderParts
+        }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
   ${DISH_FRAGMENT}
+  ${ORDERS_FRAGMENT}
 `;
 
 interface IParams {
@@ -46,7 +59,7 @@ export const MyRestaurant = () => {
         style={{
           backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
         }}
-      ></div>
+      />
       <div className="container p-10">
         <h2 className="text-4xl font-medium mb-10">
           {data?.myRestaurant.restaurant?.name || "Loading..."}
@@ -75,6 +88,62 @@ export const MyRestaurant = () => {
             </div>
           )}
         </div>
+        {data?.myRestaurant.restaurant?.orders.length ? (
+          <div className="mt-20 mb-10">
+            <h4 className="text-center text-2xl font-medium">Sales</h4>
+            <div className="mt-10">
+              <VictoryChart
+                height={500}
+                theme={VictoryTheme.material}
+                width={window.innerWidth}
+                domainPadding={50}
+                containerComponent={<VictoryVoronoiContainer />}
+              >
+                <VictoryLine
+                  labels={({ datum }) => `$${datum.y}`}
+                  labelComponent={
+                    <VictoryTooltip
+                      style={{ fontSize: 18 } as any}
+                      renderInPortal
+                      dy={-20}
+                    />
+                  }
+                  data={data?.myRestaurant.restaurant?.orders.map((order) => {
+                    return {
+                      x: order.createdAt,
+                      y: order.total,
+                    };
+                  })}
+                  interpolation="natural"
+                  style={{
+                    data: {
+                      strokeWidth: 5,
+                    },
+                  }}
+                />
+                {/* <VictoryAxis
+                style={{
+                  tickLabels: {
+                    fontSize: 18,
+                    fill: "#4D7C0F",
+                  } as any,
+                }}
+                dependentAxis
+                tickFormat={(tick) => `$${tick}`}
+              /> */}
+                <VictoryAxis
+                  tickLabelComponent={<VictoryLabel renderInPortal />}
+                  style={{
+                    tickLabels: {
+                      fontSize: 20,
+                    } as any,
+                  }}
+                  tickFormat={(tick) => new Date(tick).toLocaleDateString("en-US")}
+                />
+              </VictoryChart>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
